fa9da1ca33095ecd0cef70aee883c09b7782488b
anjuachulekshmi <anjuachulekshmi836@gmail.com>
2022-05-19 04:34:59 +0530

    addemployee coded

---
+++ src/Component/Addemployee.js
@@ -0,0 +1,60 @@
+import React from 'react'
+
+const Addemployee = () => {
+  return (
+    
+          <div>
+        
+        <div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+                <div className="col  col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label for="" className="form-label">Employee ID</label>
+                    <input type="text" className="form-control"/>
+                </div>
+               
+                <div className="col  col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label for="" className="form-label">Employee name</label>
+                    <input type="text" className="form-control"/>
+                </div>
+                <div className="col  col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label for="" className="form-label">Designation</label>
+                    <input type="text" className="form-control"/>
+
+                </div>
+                <div className="col  col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label for="" className="form-label">Salary</label>
+                    <input type="text" className="form-control"/>
+                    </div>
+
+                    <div className="col  col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" className="form-label">Company Name</label>
+                    <input type="text" className="form-control"/>
+
+                </div>
+
+                <div className="col  col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label for="" className="form-label">DOB</label>
+                    <input type="text" className="form-control"/>
+                    </div>
+
+                    <div className="col  col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label for="" className="form-label">Email</label>
+                    <input type="text" className="form-control"/>
+                    </div>
+               
+               
+                <div className="col  col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <button className="btn btn-success">SUBMIT</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    </div>
+  )
+}
+
+export default Addemployee
